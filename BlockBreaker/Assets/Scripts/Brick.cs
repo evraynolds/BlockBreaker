b19a8ccaf3837c6b2ea039eeb3a1137dfae1d210
ALPHA\Evan <raynoldsevan@gmail.com>
2018-03-01 17:07:20 +0000

    Initial Commit

---
+++ BlockBreaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Brick : MonoBehaviour {
+
+    public int maxHits;
+    private int timesHit;
+    private LevelManager levelManager;
+	// Use this for initialization
+	void Start () {
+        timesHit = 0;
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        timesHit++;
+        //SimulateWIn();
+        if (timesHit == maxHits)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //TODO 
+    void SimulateWIn()
+    {
+        SceneManager.LoadScene("Win");
+    }
+}
---
+++ BlockBreaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+    private LevelManager levelManager;
+    void Start()
+    {
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+    }
+    //private void OnCollisionEnter2D(Collision2D collision){}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        levelManager.LoadLevel("Lose");
+    }
+}
